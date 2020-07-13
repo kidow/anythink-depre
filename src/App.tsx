c3908c69a451236f9c82db41b1a4b9194747478e
kidow <wcgo2ling@gmail.com>
2020-07-14 01:27:21 +0900

    기본 UI 및 아이콘 세팅

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,41 @@
-import React from 'react'
+import React, { useState } from 'react'
+import CopyToClipboard from 'react-copy-to-clipboard'
+import CopyIcon from './copy-regular.svg'
+import RedoIcon from './redo-solid.svg'
+import GithubIcon from './github-brands.svg'
 
 interface Props {}
-interface State {}
 
 const App: React.FunctionComponent<Props> = () => {
-  return <>App</>
+  const localValue = localStorage.getItem('anythink')
+  const [anythink, setAnythink] = useState<string>(localValue || '')
+  return (
+    <>
+      <textarea
+        placeholder="아무 생각..."
+        autoFocus
+        value={anythink}
+        onChange={(e) => setAnythink(e.target.value)}
+        autoCapitalize="off"
+        autoComplete="off"
+      />
+      <img
+        src={GithubIcon}
+        alt="github"
+        className="github"
+        onClick={() => window.open('https://github.com/kidow/anythink')}
+      />
+      <CopyToClipboard text={anythink}>
+        <img src={CopyIcon} alt="clipboard" className="clipboard" />
+      </CopyToClipboard>
+      <img
+        src={RedoIcon}
+        alt="redo"
+        className="redo"
+        onClick={() => setAnythink('')}
+      />
+    </>
+  )
 }
 
 export default App
